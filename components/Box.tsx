e3fba59d56bbe0f032fcf6eafb2ceb0397afe8f1
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-10 18:50:47 +0100

    feat: Add the box component

---
+++ components/Box.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge';
+
+type Props = {
+  children: ReactNode,
+  className?: string; 
+}
+
+const Box = ({ children, className }: Props) => {
+  return (
+    <>
+      <div className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}>
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Box
