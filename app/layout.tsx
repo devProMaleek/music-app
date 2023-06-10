1434f7e7b368bdee25f29223899e02206bbbf21d
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-10 18:52:20 +0100

    feat: Add the sidebar component to the layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import './globals.css'
-import { Figtree } from 'next/font/google'
+import Sidebar from '@/components/Sidebar';
+import './globals.css';
+import { Figtree } from 'next/font/google';
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Music App',
   description: 'A spotify clone created by Abdulmalik Adebayo (devProMaleek)',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
