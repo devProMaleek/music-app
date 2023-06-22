3072445af303472f6bb1339f322fc2ead9e5bc01
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-22 23:25:12 +0100

    refactor: create the button component

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import React, { ButtonHTMLAttributes, forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ className, children, disabled, type = 'button', ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={twMerge(
+          `w-full rounded-full bg-green-500 border border-transparent px-3 py-2 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`,
+          className
+        )}
+        disabled={disabled}
+        type={type}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;
