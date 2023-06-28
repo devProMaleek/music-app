b2d5f0c51e4b7702a4acd605f546c81d81f8d138
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-28 23:28:39 +0100

    refactor:Add the header componnent

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import ListItem from '@/components/ListItem';
 import Image from 'next/image';
 
 export default function Home() {
@@ -6,7 +7,12 @@ export default function Home() {
     <>
       <div className="w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
         <Header>
-          Header
+          <div className="mb-2">
+            <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+              <ListItem image='/images/liked.png' href='liked' name='Liked Songs' />
+            </div>
+          </div>
         </Header>
       </div>
     </>
