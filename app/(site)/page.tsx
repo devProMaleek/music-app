936c9c9749e2e43788e4c795eb56d1fc7e6ad85e
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-06-30 22:40:56 +0100

    feat: Finish the layout of the landing page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,6 +14,16 @@ export default function Home() {
             </div>
           </div>
         </Header>
+        <div className="mt-2 mb-7 px-6">
+          <div className="flex justify-between items-start">
+            <h1 className="text-white text-2xl font-semibold">
+              Latest Songs
+            </h1>
+          </div>
+          <div className="">
+            List of Songs
+          </div>
+        </div>
       </div>
     </>
   );
