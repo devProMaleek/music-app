6c6670c0dfca00aedfee161b9dee2fbf848f7c9b
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-17 21:46:57 +0100

    refactor: Add the Toaster Provider component
    Add the toaster provider component and refactored the import section of the file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,13 @@ import Sidebar from '@/components/Sidebar';
 import { Metadata } from 'next';
 import './globals.css';
 import { Figtree } from 'next/font/google';
+
+
+import ToasterProvider from '@/providers/ToasterProvider';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import UserProvider from '@/providers/UserProvider';
 import ModalProvider from '@/providers/ModalProvider';
+
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -17,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
